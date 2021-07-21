713b20dcdf97e454031d4968039981cc83ea3a51
Anastasiia <bozhenko_696@mail.ru>
2021-07-21 12:11:08 +0400

    Added image to default value of the modals

--- app/models/product-list.js
+++ app/models/product-list.js
@@ -11,7 +11,7 @@ export const ProductsListModel = Backbone.Model.extend({
           value: '200',
           currency: 'USD'
         },
-        img: ''
+        img: 'https://images.unsplash.com/photo-1598440947619-2c35fc9aa908?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'
       }
     ]
   }
